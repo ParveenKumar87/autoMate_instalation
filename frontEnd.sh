f967edc71b0fa878a20bb8e987f89f945aebc2d2
Lenovo <parveen.pk87@gmail.com>
2024-06-08 22:15:09 -0400

    copy expense file step changed

--- frontEnd.sh
+++ frontEnd.sh
@@ -1,8 +1,8 @@
-cp expense.conf /etc/nginx/default.d/expense.conf
 dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+cp expense.conf /etc/nginx/default.d/expense.conf
 systemctl enable nginx
 systemctl restart nginx
