d1e8a049bf68ed28ded9ba5b3a4885c8d3ab1a6b
Lenovo <parveen.pk87@gmail.com>
2024-06-08 20:52:48 -0400

    FrontEndScript

---
+++ frontEnd.sh
@@ -0,0 +1,8 @@
+cp expense.conf /etc/nginx/default.d/expense.conf
+dnf install nginx -y
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl enable nginx
+systemctl restart nginx
