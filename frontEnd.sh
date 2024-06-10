9a9a7d27512e3c9ba7812383515540ef7678f404
Lenovo <parveen.pk87@gmail.com>
2024-06-09 23:39:11 -0400

    Code replaced with functions

--- frontEnd.sh
+++ frontEnd.sh
@@ -1,11 +1,21 @@
-echo -e "\e[35mInstalling nginx Server\e[0m"
+source common.sh
+HEADING Installing nginx Server
 dnf install nginx -y
 echo exit status - $?
 
+HEADING Removing old content
 rm -rf /usr/share/nginx/html/*
+
+HEADING Downloading source code from internet
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+HEADING Copying configuration file
 cp expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl enable nginx
+
 systemctl restart nginx
+echo exit status - $?
