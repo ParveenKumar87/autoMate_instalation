78866d22189925a52936dbfc0d483ab9c9a1ece3
Lenovo <parveen.pk87@gmail.com>
2024-06-09 22:37:57 -0400

    copy expense file step changed

--- backEnd.sh
+++ backEnd.sh
@@ -2,6 +2,7 @@ cp backend.service /etc/systemd/system/backend.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+useradd expense
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
--- frontEnd.sh
+++ frontEnd.sh
@@ -1,4 +1,7 @@
+echo -e "\e[35mInstalling nginx Server\e[0m"
 dnf install nginx -y
+echo exit status - $?
+
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
