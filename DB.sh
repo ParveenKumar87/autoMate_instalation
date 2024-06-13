8cf51ffe388dbd27edf6ece46023b0cd67c65003
Lenovo <parveen.pk87@gmail.com>
2024-06-13 00:04:40 -0400

    Code replaced with functions

--- DB.sh
+++ DB.sh
@@ -1,4 +1,19 @@
+source common.sh
+rm -f /tmp/expense.log
+
+if [ -z "$1" ];then
+  echo "Enter the password for MySQL"
+fi
+
+HEADING Installing MySQL Server
 dnf install mysql-server -y
+echo exit status - $?
+
+HEADING enabling and starting MySQL Service
 systemctl enable mysqld
 systemctl start mysqld
+echo exit status - $?
+
+HEADING Installing MySQL Server
 mysql_secure_installation --set-root-pass $1
+echo exit status - $?
