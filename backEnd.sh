56d1556b0bd89d0e3b81aab7a0a1980cc9488c15
Lenovo <parveen.pk87@gmail.com>
2024-06-13 00:37:51 -0400

    Code replaced with functions

--- backEnd.sh
+++ backEnd.sh
@@ -1,10 +1,25 @@
+source common
+
 cp backend.service /etc/systemd/system/backend.service
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
+
+HEADING "Installing NodeJS"
 dnf install nodejs -y
-useradd expense
+STAT $?
+
+HEADING "Installing user Expense"
+id expense &>>/tmp/expense.log
+  if [ $? -ne 0 ]; then
+    useradd expense &>>/tmp/expense.log
+  fi
+STAT $?
 mkdir /app
+
+HEADING "Downloading app code from Server"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+STAT $?
+
 cd /app
 unzip /tmp/backend.zip
 cd /app
@@ -12,5 +27,8 @@ npm install
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
+
+HEADING "Installing MySQL client"
 dnf install mysql -y
 mysql -h 23.22.204.199 -uroot -p$1 < /app/schema/backend.sql
+STAT $?
